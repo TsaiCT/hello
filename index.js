ec4e92dd89a0e69bd0c9367f760d155de866cb74
TsaiCT <jason40703@gmail.com>
2015-10-12 23:39:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function showFunction(x){
- x.src="web._design/chocopa.jpg";
+ x.src="https://cloud.githubusercontent.com/assets/14838693/10431856/286e54e6-713a-11e5-9a66-648a96ae9b7a.jpg";
  x.width = "100px";
  x.height = "100px";
 }
