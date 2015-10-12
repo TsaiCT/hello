dfa7d648468808307e7e215520ce4c44cb6bd5c2
TsaiCT <jason40703@gmail.com>
2015-10-12 23:59:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 function showFunction(){
- document.getElementById("me").src="https://cloud.githubusercontent.com/assets/14838693/10431856/286e54e6-713a-11e5-9a66-648a96ae9b7a.jpg"
- document.getElementById("me").width="100px";
- document.getElementById("me").height="100px";
+ document.getElementById("me").src="https://cloud.githubusercontent.com/assets/14838693/10431856/286e54e6-713a-11e5-9a66-648a96ae9b7a.jpg";
 }
