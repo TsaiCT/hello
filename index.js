064f7e6dd003ffb4fc1865e547b44d08d3a265f3
TsaiCT <jason40703@gmail.com>
2015-10-13 00:24:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 function showFunction(){
- if(document.getElementById("me").src="https://cloud.githubusercontent.com/assets/14838693/10431856/286e54e6-713a-11e5-9a66-648a96ae9b7a.jpg")
-  document.getElementById("me").src="https://cloud.githubusercontent.com/assets/14838693/10431857/2892cfec-713a-11e5-8d06-40774c5c1f3d.png"; 
- else
-  document.getElementById("me").src="https://cloud.githubusercontent.com/assets/14838693/10431856/286e54e6-713a-11e5-9a66-648a96ae9b7a.jpg";
+ document.getElementById("me").src="https://cloud.githubusercontent.com/assets/14838693/10431856/286e54e6-713a-11e5-9a66-648a96ae9b7a.jpg";
  me.style.width= "50px";
  me.style.height= "50px";
 }
