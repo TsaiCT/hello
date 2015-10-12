e3c4d53289583f2776c7616173d54ec4b0dbf0f0
TsaiCT <jason40703@gmail.com>
2015-10-13 00:20:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function showFunction(){
  document.getElementById("me").src="https://cloud.githubusercontent.com/assets/14838693/10431856/286e54e6-713a-11e5-9a66-648a96ae9b7a.jpg";
  me.style.width= "50px";
- me.style.height= "50";
+ me.style.height= "50px";
 }
