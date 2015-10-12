17255f54f656668c20811dbe57fcab1cb73152fc
TsaiCT <jason40703@gmail.com>
2015-10-12 23:53:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function showFunction(x){
- x.style.src="https://cloud.githubusercontent.com/assets/14838693/10431856/286e54e6-713a-11e5-9a66-648a96ae9b7a.jpg";
- x.style.width = "100px";
- x.style.height = "100px";
+function showFunction(){
+ document.getElementById("me").src="https://cloud.githubusercontent.com/assets/14838693/10431856/286e54e6-713a-11e5-9a66-648a96ae9b7a.jpg"
+ document.getElementById("me").width="100px";
+ document.getElementById("me").height="100px";
 }
